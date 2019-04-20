c5ddbfb54631c156de1cc24df4693ae2600163de
isplendid <hixushichao@163.com>
2019-04-21 00:54:57 +0800

    LRUCache

---
+++ src/main/java/com/xu/leetcode/binarytree/TreeNode.java
@@ -0,0 +1,13 @@
+package com.xu.leetcode.binarytree;
+
+/**
+ * Created by sop on 2019/4/20.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
