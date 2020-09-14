dffade70927d5d0d173dc6e3a0fe0b528afde4b6
isplendid <hixushichao@163.com>
2020-09-14 18:03:13 +0800

    heapsort

--- algorithm-implementation/src/main/java/com/xu/leetcode/sort/HeapSort.java
+++ algorithm-implementation/src/main/java/com/xu/leetcode/sort/HeapSort.java
@@ -1,119 +1,148 @@
 package com.xu.leetcode.sort;
 
-class ArrayUtils{
-	public static void printArray(int[] array){
-		System.out.print("{");
-		for(int i=0; i<array.length; i++){
-			System.out.print(array[i]);
-			if(i<array.length - 1){
-				System.out.print(", ");
-			}
-		}
-		System.out.println("}");
-	}
-	
-	public static void exchangeElements(int[] array, int index1, int index2){
-		int temp = array[index1];
-		array[index1] = array[index2];
-		array[index2] = temp;
-	}
+class ArrayUtils {
+    public static void printArray(int[] array) {
+        System.out.print("{");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
+
+    public static void exchangeElements(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
 
+
+/****
+ * ¶ş²æ¶ÑµÄ²Ù×÷£º insert(ÉÏ¸¡£©£º´ÓĞ¡µ½ÉÏ²Ù×÷£¬ deleteMax (ÏÂ³Á£©£º´ÓÉÏµ½ÏÂ²Ù×÷
+ * ¶ÑÊÇÒ»ÖÖÍêÈ«¶ş²æÊ÷
+ * ²åÈëÊı¾İºÍÉ¾³ı¶Ñ¶¥ÔªËØµÄÖ÷ÒªÂß¼­¾ÍÊÇ¶Ñ»¯£¬
+ * ËùÒÔ£¬Íù¶ÑÖĞ²åÈëÒ»¸öÔªËØºÍÉ¾³ı¶Ñ¶¥ÔªËØµÄÊ±¼ä¸´ÔÓ¶È¶¼ÊÇ
+ *
+ * »ùÓÚ¶Ñ½á¹¹ÊµÏÖµÄÅÅĞòËã·¨³ÆÎª¶ÑÅÅĞò£¬ËüÊ±¼ä¸´ÔÓ¶È·Ç³£ÎÈ¶¨Îª£¬²¢ÇÒËü»¹ÊÇÔ­µØÅÅĞòËã·¨¡£
+ * Æä¹ı³Ì¿ÉÒÔ´óÖÂ·Ö½âÎªÁ½¸ö´óµÄ²½Öè£º½¨¶ÑºÍÅÅĞò
+ *
+ * ½¨¶Ñ¹ı³Ì£º ¶øµÚ¶şÖÖÊµÏÖË¼Â·£¬ÊÇ´ÓºóÍùÇ°´¦ÀíÊı×é£¬²¢ÇÒÃ¿¸öÊı¾İ¶¼ÊÇ´ÓÉÏÍùÏÂ¶Ñ»¯
+ * https://www.zdaiot.com/DataStructureAlgorithm/27%E5%A0%86%E5%92%8C%E5%A0%86%E6%8E%92%E5%BA%8F%EF%BC%9A%E4%B8%BA%E4%BB%80%E4%B9%88%E8%AF%B4%E5%A0%86%E6%8E%92%E5%BA%8F%E6%B2%A1%E6%9C%89%E5%BF%AB%E9%80%9F%E6%8E%92%E5%BA%8F%E5%BF%AB/
+ *
+ *
+ */
+
+/***
+ * ¶ÑÅÅĞòÊ±¼ä¸´ÔÓ¶ÈÒ»°ãÈÏÎª¾ÍÊÇO(nlogn)¼¶
+ * ¿Õ¼äO(1£©
+ */
 public class HeapSort {
-public static void main(String[] args) {
-	int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3};
-	System.out.println("Before heap: ");
-	ArrayUtils.printArray(array);
-	
-	heapSort(array);
-	
-	System.out.println("After heap sort: ");
-	ArrayUtils.printArray(array);
-}
+    public static void main(String[] args) {
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3};
+        System.out.println("Before heap: ");
+        ArrayUtils.printArray(array);
 
-private static  void heapSort(int[] array) {
-	if(array == null || array.length == 1){
-		return;
+        heapSort(array);
+
+        System.out.println("After heap sort: ");
+        ArrayUtils.printArray(array);
+    }
+
+    private static void heapSort(int[] array) {
+        if (array == null || array.length == 1) {
+            return;
+        }
+        //step 1:  ¹¹Ôì´ó¶¥¶Ñ
+        buildMaxHeap(array);
+
+        //step2: ½»»»¶Ñ¶¥ÔªËØÓëÄ©Î²ÔªËØ + µ÷Õû¶Ñ½á¹¹
+        for (int i = array.length - 1; i >= 1; i--) {
+            ArrayUtils.exchangeElements(array, 0, i);  //½«¶Ñ¶¥ÔªËØÓëÄ©Î²ÔªËØ½øĞĞ½»»»
+            ajustHeap(array,0, i);  //ÖØĞÂ¶Ô¶Ñ½øĞĞµ÷Õû
+        }
+    }
+
+	//³õÊ¼»¯×î´ó¶Ñ
+	private static void buildMaxHeap(int[] array) {
+		if (array == null || array.length <= 1) {
+			return;
+		}
+		int half = array.length / 2 - 1; //´ÓµÚÒ»¸ö·ÇÒ¶×Ó½áµã´ÓÏÂÖÁÉÏ£¬´ÓÓÒÖÁ×óµ÷Õû½á¹¹
+		for (int i = half; i >= 0; i--) {
+			ajustHeap(array, i, array.length);
+		}
 	}
-	buildMaxHeap(array);
-	
-	for(int i = array.length - 1; i >= 1; i--){
-		ArrayUtils.exchangeElements(array, 0, i);
-		
-		heapAdjust(array, i,0);
+
+
+	/***
+	 *  µ÷Õû´ó¶¥¶Ñ£¨½öÊÇµ÷Õû¹ı³Ì£¬½¨Á¢ÔÚ´ó¶¥¶ÑÒÑ¹¹½¨µÄ»ù´¡ÉÏ£© ´ÓÉÏµ½ÏÂ
+     *  ĞÔÖÊÒ»: Ë÷ÒıÎªiµÄ×óº¢×ÓµÄË÷ÒıÊÇ (2*i+1);
+     * ĞÔÖÊ¶ş: Ë÷ÒıÎªiµÄ×óº¢×ÓµÄË÷ÒıÊÇ (2*i+2);
+     * ĞÔÖÊÈı: Ë÷ÒıÎªiµÄ¸¸½áµãµÄË÷ÒıÊÇ floor((i-1)/2);
+	 * @param array
+	 * @param heapSize  ´ıÅÅĞò¶Ñ³¤¶È
+	 * @param index µ±Ç°ÔªËØË÷Òı
+	 */
+    private static void ajustHeap(int[] array, int index, int heapSize) {
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+
+        int largest = index;
+        if (left < heapSize && array[left] > array[index]) {
+            largest = left;
+        }
+        if (right < heapSize && array[right] > array[largest]) {
+            largest = right;
+        }
+        if (index != largest) {
+            ArrayUtils.exchangeElements(array, index, largest);
+            ajustHeap(array, largest, heapSize);
+        }
+    }
+
+
+
+
+
+
+
+
+
+	//µ÷Õû×î´ó¶Ñ
+	public void heapify(int[] arr, int index, int heapSize) {
+		int left = index * 2 + 1;
+		int right = index * 2 + 2;
+		int largest = index;
+
+		while (left < heapSize) {
+			if (arr[left] > arr[index]) {
+				largest = left;
+			}
+
+			if (right < heapSize && arr[right] > arr[largest]) {
+				largest = right;
+			}
+
+			if (largest != index) {
+				swap(arr, index, largest);
+			} else {
+				break;
+			}
+			index = largest;
+			left = index * 2 + 1;
+			right = index * 2 + 2;
+		}
 	}
-}
 
-private static void heapAdjust(int[] array, int heapSize, int index) {
-	int left = index*2 +1;  //indexå¨‘æ’¶å„¤é—æ’®æ‡éŒæ»ƒä»¯é–»ã„¥å«¬é™å‰§ï¿½éæ‰®ç§´ç¼‚å†¿æ‹·
-    int right = index*2 +2;
-    
-    int largest = index;
-    if(left < heapSize && array[left] > array[index]){
-    	largest = left;
-    }
-    
-    if(right < heapSize && array[right] > array[largest]){
-    	largest = right;
-    }
-    
-    if(index != largest){
-    	ArrayUtils.exchangeElements(array, index, largest);
-    	
-    	heapAdjust(array, heapSize,largest);
-    }
-	
-	
-}
 
-//ç€µã‚…æ«é¨å«æ…¨ç€£î„î‡§  ---å©¢å †å›¥ï¿½é–¸î‚¬æ‹·--
-private static void buildMaxHeap(int[] array) {
-    if(array == null || array.length <=1 ){
-    	return;
-    }
-    
-    int half = array.length/2;
-    for(int i = half; i>=0; i--){
-    	heapAdjust(array, array.length,i);
-    }
-    	
-}	
-    
-    
-  //è°ƒæ•´æœ€å¤§å †~~
-    public void heapify(int[]  arr, int index, int heapSize){
-    	int left = index * 2 + 1;
-    	int right = index * 2 + 2;
-    	int largest = index;
-    	
-    	while(left < heapSize){
-    		if(arr[left]>arr[index]){
-    			largest = left;
-    		}
-    		
-    		if(right <heapSize && arr[right]>arr[largest]){
-    			largest = right;
-    		}
-    		
-    		if(largest != index){
-    			swap(arr, index, largest);
-    		}else{
-    			break;
-    		}
-    		index = largest;
-    		left = index*2 +1;
-    		right = index *2 +2;
-    	}
-    }
-    
-    
- private   void swap(int[] arr, int parent, int index) {
-    	int tmp = arr[parent];
-    	arr[parent] = arr[index];
-    	arr[index] = tmp;
-    	
-    }
-
+	private void swap(int[] arr, int parent, int index) {
+		int tmp = arr[parent];
+		arr[parent] = arr[index];
+		arr[index] = tmp;
+	}
 
 
 }
